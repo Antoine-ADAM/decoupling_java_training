4232b317f086817a2f6e02b3552eaefb403980c8
Antoine ADAM <flancraft10@gmail.com>
2022-10-24 16:26:26 +0200

    P7

--- src/main/java/fr/lernejo/guessgame/HumanPlayer.java
+++ src/main/java/fr/lernejo/guessgame/HumanPlayer.java
@@ -7,7 +7,7 @@ import java.util.Scanner;
 public class HumanPlayer implements Player {
     @Override
     public long askNextGuess() {
-        LoggerFactory.getLogger("HumanPlayer").log("Enter a number between 0 and 100:");
+        LoggerFactory.getLogger("player").log("Enter a number between 0 and 100:");
         Scanner scan = new Scanner(System.in);
         return scan.nextLong();
     }
--- src/main/java/fr/lernejo/logger/LoggerFactory.java
+++ src/main/java/fr/lernejo/logger/LoggerFactory.java
@@ -2,6 +2,13 @@ package fr.lernejo.logger;
 
 public class LoggerFactory {
     public static Logger getLogger(String name) {
-        return new ContextualLogger(new CompositeLogger(new FileLogger("test.log"), new ConsoleLogger()), name);
+        return
+            new ContextualLogger(new CompositeLogger(
+                new FilteredLogger(
+                    new FileLogger("test.log"),
+                    s -> !s.contains("player")
+                ),
+                new ConsoleLogger()
+        ), name);
     }
 }
