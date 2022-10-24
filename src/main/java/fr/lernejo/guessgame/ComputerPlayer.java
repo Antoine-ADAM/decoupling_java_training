7d47fc99f09e20a80ef98141ebf1d00ecbb56788
Antoine ADAM <flancraft10@gmail.com>
2022-10-24 15:56:24 +0200

    P5

--- src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
+++ src/main/java/fr/lernejo/guessgame/ComputerPlayer.java
@@ -1,12 +1,17 @@
 package fr.lernejo.guessgame;
 
+import fr.lernejo.logger.Logger;
+import fr.lernejo.logger.LoggerFactory;
+
 public class ComputerPlayer implements Player {
     private long min = 0;
     private long max = 100;
     private long guess = 50;
+    private final Logger logger = LoggerFactory.getLogger("ComputerPlayer");
 
     @Override
     public long askNextGuess() {
+        this.logger.log("Guessing " + this.guess);
         return guess;
     }
 
